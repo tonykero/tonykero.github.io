efb0662fc584670c9020b6711e7928d6153d4822
tony_kero <antoine.karcher.id@gmail.com>
2018-12-30 21:52:40 +0100

    init docute, mermaid, katex

---
+++ index.js
@@ -0,0 +1,54 @@
+new Docute({
+    target: '#docute',
+    plugins: [
+        docuteKatex(),
+        docuteMermaid()
+    ],
+    sourcePath: "./",
+    nav: [
+        {
+            title: "Home",
+            link: "/"
+        },
+        {
+            title: "GitHub",
+            link: "https://github.com/tonykero"
+        }
+    ],
+    sidebar: [
+        {
+            title: "About",
+            links: [
+                {
+                    title: "Thoses pages",
+                    link: "/about"
+                },
+                {
+                    title: "GitHub",
+                    link: "/github"
+                }
+            ]
+        },
+        {
+            title: "Projects",
+            links: [
+                {
+                    title: "Status",
+                    link: "/projects/status"
+                },
+                {
+                    title: "Compote",
+                    link: "/projects/compote"
+                },
+                {
+                    title: "Moe 2.0",
+                    link: "/projects/moe"
+                },
+                {
+                    title: "p2p",
+                    link: "/projects/p2p"
+                }
+            ]
+        }
+    ]
+})
