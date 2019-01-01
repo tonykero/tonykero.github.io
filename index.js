abc3261b4f07e25013505899b583b6a0033fd57b
tony_kero <antoine.karcher.id@gmail.com>
2019-01-01 18:34:00 +0100

    Dynamic sidebar & navbar

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
+const sourcePath = "https://tonykero-pages-cdn.surge.sh"
+const config = { nav: null, sidebar: null }
+Vue.http.get(sourcePath + '/config.json').then(res => {
+    return res.json();
+}).then(json => {
+    config = json;
+})
+
 new Docute({
     target: '#docute',
     plugins: [
         docuteKatex(),
         docuteMermaid()
     ],
-    sourcePath: "https://tonykero-pages-cdn.surge.sh",
-    nav: [
-        {
-            title: "Home",
-            link: "/"
-        },
-        {
-            title: "GitHub",
-            link: "https://github.com/tonykero"
-        }
-    ],
-    sidebar: [
-        {
-            title: "About",
-            links: [
-                {
-                    title: "Thoses pages",
-                    link: "/about"
-                },
-                {
-                    title: "GitHub",
-                    link: "/github"
-                }
-            ]
-        },
-        {
-            title: "Projects",
-            links: [
-                {
-                    title: "Status",
-                    link: "/projects/status"
-                },
-                {
-                    title: "Compote",
-                    link: "/projects/compote"
-                },
-                {
-                    title: "Moe 2.0",
-                    link: "/projects/moe"
-                },
-                {
-                    title: "p2p",
-                    link: "/projects/p2p"
-                }
-            ]
-        }
-    ]
+    sourcePath,
+    nav: config.nav,
+    sidebar: config.sidebar
 })
